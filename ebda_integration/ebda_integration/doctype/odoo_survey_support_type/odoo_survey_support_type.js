4031c53526379a8c94d82860783021f8d11e7632
KerollesFathy <kerolles.f@outlook.com>
2024-03-09 21:48:26 +0200

    added odoo_survey_support_type &  ebda_survey & ebda_survey_answers

---
+++ ebda_integration/ebda_integration/doctype/odoo_survey_support_type/odoo_survey_support_type.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024,  Axentor LLC and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Odoo Survey Support Type', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ ebda_integration/ebda_integration/doctype/odoo_survey_support_type/odoo_survey_support_type.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2024,  Axentor LLC and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class OdooSurveySupportType(Document):
+	pass
